eeb40d3880ca3e11fdbdf15e291a827e3cbf2bf4
tharvey <tharveyster@gmail.com>
2021-08-09 09:55:48 -0400

    Change port number for testing purposes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require("fs");
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.port || 8080;
 
 const app = express();
 
